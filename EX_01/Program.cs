bd7b84ff4ffd89c0fa9347234fb0c8085f99467a
DimaSannikov <dimasannikov@gmail.com>
2023-02-24 00:33:23 +0500

    changed EX_01

---
+++ EX_01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Команда вывода в консоль
+// Console.WriteLine("Hello, GeekBrains!");
+// Console.WriteLine(100 + 1);
+
+// Пользователь вводит, программа толжна выводить это число
+// умноженное само на себя
+
+// Console.Write("Введите целое число: ");
+// int digit = int.Parse(Console.ReadLine());
+
+// int result = digit * digit;
+// Console.WriteLine($"Квадрат входного числа равен {result}");
+
+Console.WriteLine("Введите целое число 1: ");
+int digit_1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите целое число 1: ");
+int digit_2 = int.Parse(Console.ReadLine());
+
+int result = digit_2 * digit_2;
+
+if(digit_1 == result)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
